36f19ac01fb4ab202d857007dfd052d9cb26537d
Miklós Kisfaludy <kismiki2000@gmail.com>
2025-02-18 18:44:33 +0100

    base structure

---
+++ vue-app/src/router/index.js
@@ -0,0 +1,19 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  // egyéb útvonalak...
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
